83609866fb40807e66f1d53513d603077b541d8a
paolobasi02 <124430870+paolobasi02@users.noreply.github.com>
2024-01-29 18:24:10 +0100

    Prototipo aggiornato Smaterializzatore

---
+++ Proj_GlyphSeeker/Assets/Script/Paolo/Smaterializzatore.cs
@@ -0,0 +1,159 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Smaterializzatore : MonoBehaviour
+{
+    public Transform raycastStartPoint;
+    public float maxRaycastDistance = 10f; //  variabile per la massima distanza del raycast
+    public float minProjectileForce = 0f;
+    public float projectileForce = 20f;
+    public Transform spawnPoint;
+    private GameObject hiddenObject;
+    public GameObject ImageObjectCollected;
+    private GameObject hitObject;
+    
+    private void Start()
+    {
+        ImageObjectCollected.SetActive(false);
+    }
+    void Update()
+    {
+        // Memorizza l'oggetto colpito
+        GameObject hitObject = null;
+
+        // Lanciare un raycast in avanti solo se l'oggetto nascosto è null
+        if (hiddenObject == null)
+        {
+            RaycastHit hit;
+            // Aggiunto maxRaycastDistance al raycast
+            if (Physics.Raycast(raycastStartPoint.position, raycastStartPoint.forward, out hit, maxRaycastDistance))
+            {
+                hitObject = hit.transform.gameObject;
+            }
+        }
+
+        // Input per sparare
+        if (Input.GetMouseButtonDown(0))
+        {
+            // Se c'è un oggetto nascosto, spara senza dover colpire nulla con il raycast
+            if (hiddenObject != null)
+            {
+                Shoot(projectileForce);
+            }
+            // Se non c'è un oggetto nascosto, spara solo se il raycast ha colpito qualcosa
+            else if (hitObject != null)
+            {
+                Shoot(projectileForce);
+            }
+
+            if (hiddenObject == null)
+            {
+                HideObject(hitObject);
+            }
+        }
+
+        // Input per far scomparire l'oggetto
+        if (Input.GetMouseButtonDown(1))
+        {
+            if (hiddenObject != null)
+            {
+                PlaceObject(minProjectileForce);
+            }
+        }
+    }
+    void PlaceObject(float minProjectileForce)
+    {
+        // Memorizza l'oggetto colpito
+        GameObject hitObject = null;
+
+        // Lanciare un raycast in avanti solo se l'oggetto nascosto è null
+        if (hiddenObject != null)
+        {
+            RaycastHit hit;
+
+            // Ottenere il componente Rigidbody dell'oggetto clonato
+            Rigidbody projectileRb = hiddenObject.GetComponent<Rigidbody>();
+
+            // Aggiungi una forza in avanti all'oggetto clonato
+            if (projectileRb != null)
+            {
+                projectileRb.angularVelocity = Vector3.zero;
+                projectileRb.velocity = Vector3.zero;
+                
+            }
+
+
+            // Aggiunto maxRaycastDistance al raycast
+            if (Physics.Raycast(raycastStartPoint.position, raycastStartPoint.forward, out hit, maxRaycastDistance))
+            {
+                GameObject goHit = hit.transform.GetComponent<GameObject>();
+                Debug.Log(hit.transform.name);
+                if (goHit == null)
+                {
+                    hitObject = hit.transform.gameObject;
+                    Destroy(hiddenObject.GetComponent<Smaterializzatore>());
+                    hiddenObject.SetActive(true);
+                    hiddenObject.transform.position = hit.point + new Vector3(0, 0.5f, 0);
+                    // Assegna null a hiddenObject per indicare che non c'è più un oggetto nascosto
+                    hiddenObject = null;
+
+                    // Assegna a false per far scomparire l'immagine
+                    ImageObjectCollected.SetActive(false);
+
+                }
+            }
+        }
+       
+       
+    }
+
+
+    void Shoot(float projectileForce)
+    {             
+        
+        if (hiddenObject != null)
+        {
+            // Rimuovi il componente PlayerShooting dal clone per evitare duplicati
+            Destroy(hiddenObject.GetComponent<Smaterializzatore>());
+
+            // Attiva l'oggetto clonato
+            hiddenObject.SetActive(true);
+
+            // Sposta l'oggetto clonato nella posizione e rotazione del punto di sparo
+            hiddenObject.transform.position = spawnPoint.position;
+            hiddenObject.transform.rotation = spawnPoint.rotation;
+
+            // Ottenere il componente Rigidbody dell'oggetto clonato
+            Rigidbody projectileRb = hiddenObject.GetComponent<Rigidbody>();
+
+            // Aggiungi una forza in avanti all'oggetto clonato
+            if (projectileRb != null)
+            {
+                projectileRb.angularVelocity = Vector3.zero;
+                projectileRb.velocity = Vector3.zero;
+                projectileRb.AddForce(raycastStartPoint.transform.forward * projectileForce, ForceMode.Impulse);
+            }
+
+            // Assegna null a hiddenObject per indicare che non c'è più un oggetto nascosto
+            hiddenObject = null;
+
+            //assegna a false per far scomparire l'immagine
+            ImageObjectCollected.SetActive(false);
+        }
+    }
+
+    void HideObject(GameObject objToHide)
+    {
+        if (objToHide != null && objToHide.CompareTag("toHide"))
+        {
+            // Disattiva l'oggetto colpito
+            objToHide.SetActive(false);
+
+            //attiva lo sprite a schermo
+            ImageObjectCollected.SetActive(true);
+
+            // Memorizza l'oggetto nascosto
+            hiddenObject = objToHide;
+        }
+    }
+}
