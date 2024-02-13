920377ccae5bab92e19cf844675f3803c641bf71
Pietro Poidomani <124389275+PietroPoidomani@users.noreply.github.com>
2024-02-13 12:28:51 +0100

    Fixato script proiettile esplosivo

--- Proj_GlyphSeeker/Assets/Script/Pietro/ExplosiveProj.cs
+++ Proj_GlyphSeeker/Assets/Script/Pietro/ExplosiveProj.cs
@@ -2,24 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ProjectileEffect : MonoBehaviour
+public class ExplosiveProj : MonoBehaviour
 {
 
     private Rigidbody rb;
     [SerializeField]
     private float ExplosionRadius;
-    private bool targetHit;
 
     private void Start()
     {
-        rb.GetComponent<Rigidbody>();
+        rb=GetComponent<Rigidbody>();
     }
 
-    private void OnCollisionEnter(Collision collision)
+    private void OnTriggerEnter(Collider collider)
     {
         Collider[] TargetsHit = Physics.OverlapSphere(rb.transform.position, ExplosionRadius);
         foreach (var col in TargetsHit)
         {
+            //Raycast per vedere se l'esplosione ha colpito il nemico e non un muro.
+            
             if (col.gameObject.CompareTag("DestructableObject"))
             {
                 Destroy(col.gameObject);
