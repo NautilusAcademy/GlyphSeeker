0f9a2941ac750edbd2409a6057f53bc85c0e744c
lorenzomusumeci <124389907+lorenzomusumeci@users.noreply.github.com>
2024-02-14 17:12:27 +0100

    Modifiche barile
    Il barile ora quando esplode oltre a far danno ai nemici carica pure gli oggetti attorno a lui. Ho aggiunto anche un prefab chiamato SmBarile che deve essere usato quando il barile viene raccolto prima dell'esplosione

--- Proj_GlyphSeeker/Assets/Cartelle_Provvisorie/Tes_Runa_Gialla/Script/Generator.cs
+++ Proj_GlyphSeeker/Assets/Cartelle_Provvisorie/Tes_Runa_Gialla/Script/Generator.cs
@@ -4,23 +4,21 @@ using UnityEngine;
 
 public class Generator : MonoBehaviour, IChargable
 {
+    [SerializeField]
     private int charge = 0;
     private int maxCharge = 1;
 
     public ParticleSystem particleSystem;
     public GameObject door;
 
-    private void Update()
-    {
-        if(charge >= maxCharge)
-        {
-            FullCharged();
-        }
-    }
-
     public void Charge()
     {
         charge++;
+
+        if (charge >= maxCharge)
+        {
+            FullCharged();
+        }
     }
 
     public void FullCharged()
