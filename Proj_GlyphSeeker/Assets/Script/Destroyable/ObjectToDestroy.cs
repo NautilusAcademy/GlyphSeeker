0a71ba2f07e194a1f5abf76d978a9b28b485f479
Green-Hat-fd <108188257+Green-Hat-fd@users.noreply.github.com>
2024-02-23 19:57:37 +0100

    Update ObjectToDestroy.cs

--- Proj_GlyphSeeker/Assets/Script/Destroyable/ObjectToDestroy.cs
+++ Proj_GlyphSeeker/Assets/Script/Destroyable/ObjectToDestroy.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ObjectToDestroy : MonoBehaviour //, IDestroyable
+public class ObjectToDestroy : MonoBehaviour, IDestroyable
 {
     [SerializeField] bool requireExplosion = true;
 
