9a46d40d95f109c88773788aa186854b0396b5cd
lorenzomusumeci <124389907+lorenzomusumeci@users.noreply.github.com>
2024-03-04 16:46:10 +0100

    Squashed commit of the following:
    commit ec676654e1747781daee68c31be5b8786c49a361
    Author: Green-Hat-fd <108188257+Green-Hat-fd@users.noreply.github.com>
    Date:   Mon Mar 4 16:40:53 2024 +0100
    
        Update ShieldRune (piccole sistemazioni)
    
    commit 901ed3136353e9542f4a96b714442ff91176d7ac
    Author: Green-Hat-fd <108188257+Green-Hat-fd@users.noreply.github.com>
    Date:   Mon Mar 4 16:26:41 2024 +0100
    
        Aggiornata posizione
    
    commit 4f55fd3e82b4bd01324317221919d08d499c330d
    Author: Green-Hat-fd <108188257+Green-Hat-fd@users.noreply.github.com>
    Date:   Wed Feb 28 12:06:56 2024 +0100
    
        Update con var. modello
    
    commit e1ff5de4322237703f63f6e192d00b7d8f6efb60
    Author: Green-Hat-fd <108188257+Green-Hat-fd@users.noreply.github.com>
    Date:   Sun Feb 25 12:11:58 2024 +0100
    
        Update ShieldRune.cs
    
    commit 2018f26d68019eb85c710eba4d7a4a88af1d15a5
    Author: Green-Hat-fd <108188257+Green-Hat-fd@users.noreply.github.com>
    Date:   Sun Feb 25 12:11:06 2024 +0100
    
        Aggiunto Script scudo

---
+++ Proj_GlyphSeeker/Assets/Script/Player/Runes/ShieldRune.cs
@@ -0,0 +1,161 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+//using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(Collider))]
+public class ShieldRune : MonoBehaviour//PlayerShoot
+{
+    //[SerializeField] PlayerRBMovement movemScr;
+    [SerializeField] GameObject shieldModel;
+    [SerializeField] Camera playerCam;
+    Transform playerCam_Tr;
+    [Min(0)]
+    [SerializeField] float distance;
+
+    [Space(10)]   //Sezione da mettere in PlayerShoot
+    [SerializeField] int maxShieldHp;
+    int shieldHp;
+
+    [Space(10)]
+    [Min(0.1f)]
+    [SerializeField] float maxParryTime = 1;
+    [Min(0.1f)]
+    [SerializeField] float velLoseAmmo = 1;
+    bool canParry = false;
+    float currentParryTime = 0,
+          currentOpenTime = 0;
+
+    [Space(10)]
+    [SerializeField] float knockbackForce_shield = 2.5f;
+
+
+
+
+    private /*override*/ void Start()
+    {
+        shieldHp = maxShieldHp;
+        playerCam_Tr = playerCam.transform;
+
+        //base.Start();
+
+        currentParryTime = 0;
+    }
+
+    void Update()
+    {
+        //InputAction inputShield = GameManager.inst.inputManager.Player.Fire;
+        //            inputReload = GameManager.inst.inputManager.Player.Aim;
+
+
+        bool isShieldActive = false, //inputShield.ReadValue<float>() > 0,
+             isShieldTriggered = false; //inputShield.triggered;
+
+
+
+        if (isShieldActive)
+        {
+            #region Parry
+
+            //Attiva il Parry una volta
+            //quando si preme il pulsante nel primo frame
+            if (isShieldTriggered)
+            {
+                canParry = true;
+            }
+
+            if (currentParryTime >= maxParryTime)
+            {
+                //Se finisce il timer,
+                //toglie la possibilita' di Parry
+                canParry = false;
+            }
+            else
+            {
+                //Aumenta il timer se esso NON ha finito
+                currentParryTime += Time.deltaTime;
+            }
+
+            #endregion
+
+
+            #region Diminuzione dei proiettili col passare del tempo
+
+            //Aumenta il "timer" da quando e' stato aperto
+            currentOpenTime += Time.deltaTime;
+
+            //Dopo tot tempo, toglie ammo allo scudo
+            if(currentOpenTime >= velLoseAmmo)
+            {
+                shieldHp--;
+
+                currentOpenTime = 0;
+            }
+
+            #endregion
+        }
+        else
+        {
+            //Reset del Parry quando non si preme nulla
+            canParry = false;
+
+            currentParryTime = 0;
+            currentOpenTime = 0;
+        }
+
+
+
+        //(Dis)Attiva lo scudo,
+        //solo se si tiene premuto il pulsante & ha ancora HP
+        shieldModel.SetActive(!(shieldHp > 0  &&  isShieldActive));
+
+
+        //Porta lo scudo davanti alla telecamera
+        Vector3 shieldPos = playerCam_Tr.position + playerCam_Tr.forward * distance;
+
+        shieldModel.transform.position = shieldPos;
+        shieldModel.transform.rotation = playerCam.transform.rotation;
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //IBullet bulletCheck = other.GetComponent<IEnemy>();
+
+        if (other.CompareTag("Bullet")) //(bulletCheck != null)
+        {
+            bool isFromFront = CheckFromFront(other.transform);
+
+            if (canParry  &&  isFromFront)
+            {
+                //Se lo puo' parare e il proiettile arriva da davanti,
+                //allora lo spedisce nella stessa direzione dello scudo
+        //        other.ChangeVelocity(transform.forward, 0);
+                
+                //Piccolo rinculo al giocatore
+        //        movemScr.Knockback(knockbackForce_shield);
+            }
+                
+            //Toglie munizioni allo scudo
+    //        shieldhp--;
+
+            //Distrugge il proiettile
+            Destroy(other);
+        }
+    }
+
+    /// <summary>
+    /// Calcola la direzione nel range [-1; 1]
+    /// <br></br><i>(se si avvicina a 1 ----> sono nella stessa direzione)
+    /// <br></br>(se si avvicina a -1 ---> sono in direzioni opposte)
+    /// <br></br>(se si avvicina a 0 ----> sono perpendicolari)</i>
+    /// </summary>
+    bool CheckFromFront(Transform bullet)
+    {
+        float directionRange = Vector3.Dot(bullet.forward.normalized,
+                                           transform.forward);
+
+        return directionRange < 0;
+    }
+}
