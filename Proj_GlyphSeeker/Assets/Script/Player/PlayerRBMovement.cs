6a5731097fd8327641048b653cc4494cffd2ae81
Green-Hat-fd <108188257+Green-Hat-fd@users.noreply.github.com>
2024-03-09 14:30:27 +0100

    Aggiornato Player + Movem. (come Giocatore_Finale)

--- Proj_GlyphSeeker/Assets/Script/Player/PlayerRBMovement.cs
+++ Proj_GlyphSeeker/Assets/Script/Player/PlayerRBMovement.cs
@@ -8,6 +8,8 @@ public class PlayerRBMovement : MonoBehaviour, IPlayer
     [SerializeField] float playerSpeed = 7.5f;
     [SerializeField] float jumpPower = 8.5f;
     [SerializeField] float increasedGravityMult = 3.5f;
+    [Min(0)]
+    [SerializeField] float addedJumpHeight = 1f;
     float x_movem, z_movem;
 
     Vector3 moveVector;
@@ -70,20 +72,11 @@ public class PlayerRBMovement : MonoBehaviour, IPlayer
                                            QueryTriggerInteraction.Ignore);
 
 
-
-
-        /*if (moveVector.x == 0 && moveVector.y == 0 && isOnGround)
-        {
-            //rb.velocity = new Vector3(0f, 0f, 0f);
-            moveVector = (transform.forward * z_movem - transform.right * x_movem).normalized;
-        }//*/
-
-
         #region Salto
 
         //Se tieni premuto il tasto di salto
         //e il divisore e' sotto il limite...
-        if (isJumping && jumpPower_divider <= 2.5f)
+        if (isJumping && jumpPower_divider <= addedJumpHeight+1)
         {
             if (isOnGround || canCoyote)
             {
